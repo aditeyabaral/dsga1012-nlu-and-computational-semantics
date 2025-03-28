d750e2129a96d3c1eb14c02c1085bd44fd0b3b10
aditeyabaral <aditeya.baral@gmail.com>
2025-03-28 01:16:34 -0400

    Update run commands

--- hw3/hpc/run_truthfulqa_evaluation.sh
+++ hw3/hpc/run_truthfulqa_evaluation.sh
@@ -1,5 +1,10 @@
 python truthfulqa.py facebook/opt-125m
-# python truthfulqa.py facebook/opt-350m
-# python truthfulqa.py facebook/opt-1.3b
-# python truthfulqa.py facebook/opt-2.7b
-# python truthfulqa.py facebook/opt-6.7b
+python truthfulqa.py facebook/opt-350m
+python truthfulqa.py facebook/opt-1.3b
+python truthfulqa.py facebook/opt-2.7b
+python truthfulqa.py facebook/opt-6.7b
+
+python truthfulqa.py facebook/opt-1.3b --no-demos
+# python truthfulqa.py facebook/opt-1.3b 
+python truthfulqa.py facebook/opt-1.3b --system-prompt 'Actually,' --no-demos
+python truthfulqa.py facebook/opt-1.3b --system-prompt 'Actually,'
