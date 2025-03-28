5fdf2730bf4d98955ad1c3cdc4731092ee4abeb6
aditeyabaral <aditeya.baral@gmail.com>
2025-03-28 00:54:47 -0400

    Update slurm script

--- hw3/hpc/run_truthfulqa_evaluation.sh
+++ hw3/hpc/run_truthfulqa_evaluation.sh
@@ -1,4 +1,5 @@
-# Add all your Python calls here:
 python truthfulqa.py facebook/opt-125m
-python truthfulqa.py facebook/opt-350m
-# ... etc.
+# python truthfulqa.py facebook/opt-350m
+# python truthfulqa.py facebook/opt-1.3b
+# python truthfulqa.py facebook/opt-2.7b
+# python truthfulqa.py facebook/opt-6.7b
