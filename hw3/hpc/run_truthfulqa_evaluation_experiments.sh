b7d560559fa42f45457c048e848c147295cc9a9d
Aditeya Baral <ab12057@b-19-12.c.hpc-slurm-9c75.internal>
2025-03-28 02:55:32 -0400

    Add better extra credit results

---
+++ hw3/hpc/run_truthfulqa_evaluation_experiments.sh
@@ -0,0 +1,19 @@
+python truthfulqa.py facebook/opt-125m --no-demos
+# python truthfulqa.py facebook/opt-125m
+python truthfulqa.py facebook/opt-125m --system-prompt 'Actually,' --no-demos
+python truthfulqa.py facebook/opt-125m --system-prompt 'Actually,'
+
+python truthfulqa.py facebook/opt-350m --no-demos
+# python truthfulqa.py facebook/opt-350m
+python truthfulqa.py facebook/opt-350m --system-prompt 'Actually,' --no-demos
+python truthfulqa.py facebook/opt-350m --system-prompt 'Actually,'
+
+python truthfulqa.py facebook/opt-2.7b --no-demos
+# python truthfulqa.py facebook/opt-2.7b
+python truthfulqa.py facebook/opt-2.7b --system-prompt 'Actually,' --no-demos
+python truthfulqa.py facebook/opt-2.7b --system-prompt 'Actually,'
+
+python truthfulqa.py facebook/opt-6.7b --no-demos
+# python truthfulqa.py facebook/opt-6.7b
+python truthfulqa.py facebook/opt-6.7b --system-prompt 'Actually,' --no-demos
+python truthfulqa.py facebook/opt-6.7b --system-prompt 'Actually,'
